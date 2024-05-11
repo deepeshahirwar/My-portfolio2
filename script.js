093c491c674e2a9d648c17eb964fb5581247792d
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-05-11 21:02:51 +0530

    back to top btn added

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+ 
+
+// for back to top btn 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const toTop = document.querySelector(".to-top");
+
+  window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+          toTop.classList.add("active");
+      } else {
+          toTop.classList.remove("active");
+      }
+  });
+
+  // Scroll to top when the button is clicked
+  toTop.addEventListener("click", () => {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+      });
+  });
+});
+
